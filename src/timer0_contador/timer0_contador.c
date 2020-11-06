2e7d748f13695fcbddd2d7300412f97249f51e94
Iván Montiel cardona <ivan_montiel2013@hotmail.com>
2020-11-06 00:58:13 -0600

    Auto stash before rebase of "origin/main"

---
+++ src/timer0_contador/timer0_contador.c
@@ -0,0 +1,26 @@
+#include <18F45K50.h>                                        //Para PIC18F4550 cambiar por: #include <18F4550.h>
+#use delay(internal=48Mhz)                                  //Tipo de oscilador y frecuencia dependiendo del microcontrolador
+#build(reset=0x02000,interrupt=0x02008)        //Asignación de los vectores de reset e interrupción
+#org 0x0000,0x1FFF {}
+#include <lcd.c>
+int8 cuenta;
+#int_timer0
+void timer_0()
+{
+     set_timer0(0);
+}
+void main()
+{ 
+   setup_timer_0(rtcc_ext_l_to_h | rtcc_div_1);
+   set_timer0(0);
+   enable_interrupts(int_timer0);
+   enable_interrupts(global);
+   lcd_init();
+   while(true)
+   {
+   cuenta= get_timer0();
+   lcd_gotoxy(1,1);
+   printf(lcd_putc," Cuenta= %3u ",cuenta);
+   }
+}
+
